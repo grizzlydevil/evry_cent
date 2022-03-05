69edf04796755854a7c6ca3a2769c71ea7b4e487
grizzlydevil <53172842+grizzlydevil@users.noreply.github.com>
2022-03-05 17:42:42 +0200

    set up auth urls

--- evry_Cent/urls.py
+++ evry_Cent/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('main/', include('main.urls', namespace='main'))
+    path('main/', include('main.urls', namespace='main')),
+    path('auth/', include('auth.urls', namespace='auth'))
 ]
