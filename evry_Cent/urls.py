3b37dfb840b3b7716b12597865459b55a5f4c625
grizzlydevil <53172842+grizzlydevil@users.noreply.github.com>
2022-03-30 23:04:35 +0300

    update url paths

--- evry_Cent/urls.py
+++ evry_Cent/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('goals/', include('goals.urls', namespace='goals')),
+    path('', include('goals.urls', namespace='goals')),
     path('auth/', include('authentication.urls', namespace='auth'))
 ]
--- goals/urls.py
+++ goals/urls.py
@@ -2,13 +2,12 @@ from django.urls import path, include
 
 from rest_framework import routers
 
-from .views import GoalsViewSet
+from .views import GoalsViewSet, WalletViewSet
 
 app_name = 'goals'
 
-router = routers.SimpleRouter()
-router.register('', GoalsViewSet, basename='goal')
+router = routers.DefaultRouter()
+router.register('goals', GoalsViewSet, basename='goals')
+router.register('wallet', WalletViewSet, basename='wallet')
 
-urlpatterns = [
-    path('', include(router.urls)),
-]
+urlpatterns = router.urls
