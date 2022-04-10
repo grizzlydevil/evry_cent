19fbb94b9515ebb498f882a293ad95e5ff6e6c7d
grizzlydevil <53172842+grizzlydevil@users.noreply.github.com>
2022-04-10 15:33:26 +0300

    create pocket group srlzr create,  update methods

--- goals/serializers.py
+++ goals/serializers.py
@@ -1,4 +1,4 @@
-from django.db.models import F
+from django.db.models import Q
 
 from rest_framework import serializers
 
@@ -114,14 +114,14 @@ class PocketGroupSerializer(serializers.ModelSerializer):
         model = PocketGroup
         fields = ('title', 'pockets')
 
-    # pockets = serializers.ListField(
-    #     child=serializers.IntegerField(min_value=0),
-    #     min_length=1,
-    #     max_length=10
-    # )
-
     def validate_pockets(self, value):
-        pass
+
+        # validate at least one pocket was specified
+        if len(value) < 1:
+            raise serializers.ValidationError(
+                'please specify at least one pocket'
+            )
+
         # validate all id in the list are unique
         if len(value) != len(set(value)):
             raise serializers.ValidationError(
@@ -141,8 +141,17 @@ class PocketGroupSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError('invalid pockets selected')
 
         # validate no pockets in the list are in other pocket groups
-        pockets_in_other_groups = selected_pockets.filter(
-            pocket_group__isnull=False)
+        if self.instance:
+            # if instance exists means it's an update.
+            # Pockets may exist in current instance
+            group_id = self.instance.id
+            pockets_in_other_groups = selected_pockets.filter(
+                Q(pocket_group__isnull=False) & ~Q(pocket_group=group_id)
+            )
+        else:
+            pockets_in_other_groups = selected_pockets.filter(
+                pocket_group__isnull=False)
+
         if pockets_in_other_groups.exists():
             titles = (
                 f'"{pocket.title}"' for pocket in pockets_in_other_groups
@@ -151,5 +160,35 @@ class PocketGroupSerializer(serializers.ModelSerializer):
                 f'pockets: {", ".join(titles)} belong to other groups already'
             )
 
-        # # add pocket_group Id to selected pockets
-        # selected_pockets.update(pocket_group=self.id)
+        return value
+
+    def create(self, validated_data):
+        """Selected pockets must be assigned to a newly created group"""
+        pockets = validated_data.pop('pockets')
+        pocket_ids = (pocket.id for pocket in pockets)
+        selected_pockets = Pocket.objects.filter(id__in=pocket_ids)
+
+        pocket_group = PocketGroup.objects.create(**validated_data)
+
+        # add pocket_group Id to selected pockets
+        selected_pockets.update(pocket_group=pocket_group.id)
+
+        return pocket_group
+
+    def update(self, instance, validated_data):
+        """updated pockets must be updated with new values"""
+        pockets = validated_data.pop('pockets')
+        pocket_ids = (pocket.id for pocket in pockets)
+        selected_pockets = Pocket.objects.filter(id__in=pocket_ids)
+
+        saved_pockets = instance.pockets.all()
+
+        new_pockets = selected_pockets.exclude(id__in=saved_pockets)
+        removed_pockets = saved_pockets.exclude(id__in=selected_pockets)
+
+        new_pockets.update(pocket_group=instance.id)
+        removed_pockets.update(pocket_group=None)
+
+        instance.title = validated_data.get('title', instance.title)
+
+        return instance
