625046985b8066b73742736626e70ee90729c25b
grizzlydevil <53172842+grizzlydevil@users.noreply.github.com>
2022-04-20 23:57:28 +0300

    Create income distributor viewset

--- cycles/urls.py
+++ cycles/urls.py
@@ -1,11 +1,16 @@
 from rest_framework import routers
 
-from .views import CycleViewSet, IncomeViewSet
+from .views import CycleViewSet, IncomeViewSet, IncomeDistributorViewSet
 
 app_name = 'cycles'
 
 router = routers.DefaultRouter()
 router.register('cycles', CycleViewSet, basename='cycle')
 router.register('incomes', IncomeViewSet, basename='income')
+router.register(
+    'income-distributors',
+    IncomeDistributorViewSet,
+    basename='income-distributor'
+)
 
 urlpatterns = router.urls
--- cycles/views.py
+++ cycles/views.py
@@ -1,8 +1,9 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
-from .models import Cycle, Income
-from .serializers import CycleSerializer, IncomeSerializer
+from .models import Cycle, Income, IncomeDistributor
+from .serializers import (CycleSerializer, IncomeSerializer,
+                          IncomeDistributorSerializer)
 
 
 class CycleViewSet(viewsets.ModelViewSet):
@@ -29,3 +30,27 @@ class IncomeViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         """get current users incomes"""
         return Income.objects.filter(cycle__user=self.request.user)
+
+
+class IncomeDistributorViewSet(viewsets.ModelViewSet):
+    """List view will list all distributors with specified range of cycles"""
+
+    permission_classes = (IsAuthenticated,)
+    serializer_class = IncomeDistributorSerializer
+
+    def get_queryset(self):
+        """list only the items in specified cycles"""
+        distributors = (IncomeDistributor.objects
+                        .filter(cycle__user=self.request.user))
+        if self.action == 'list':
+            cycles_params = self.request.query_params.get('cycles')
+            cycles = (
+                [pk for pk in cycles_params.split(',') if pk.isdigit()]
+                if cycles_params else []
+            )
+            distributors = (
+                IncomeDistributor.objects
+                .filter(cycle__in=cycles)
+            )
+
+        return distributors
